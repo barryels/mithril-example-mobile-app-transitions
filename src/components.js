9418ed11ad0f2b3dac810b3d9edb7bf403ba6994
Barry Els <barryels@Barrys-MacBook-Pro.local>
2017-08-20 19:50:12 +0200

    completed initial version of sliding transition between routes.

--- src/components.js
+++ src/components.js
@@ -4,6 +4,9 @@
 var m = require('mithril');
 
 
+var store = require('./store');
+
+
 var links = {
 	view: function () {
 		return m('ul', [
@@ -15,8 +18,45 @@ var links = {
 };
 
 var appSection = {
+	oninit: function (vnode) {
+		vnode.state = {
+			routePath: store.currentRoutePath,
+			childrenPrevious: vnode.children,
+			childrenNext: vnode.children,
+			classNameModifier: '',
+		};
+	},
+	onbeforeupdate: function (vnode, vnodeOld) {
+		vnode.state.childrenPrevious = vnodeOld.children;
+	},
 	view: function (vnode) {
-		return m('div.mainSection', vnode.children);
+		var className = '.appSection';
+
+		vnode.state.childrenNext = vnode.children;
+
+		if (vnode.state.routePath !== store.currentRoutePath) {
+			vnode.children = vnode.state.childrenPrevious;
+
+			if (vnode.state.routePath.length > store.currentRoutePath.length) {
+				vnode.state.classNameModifier = className + '--backward';
+			} else {
+				vnode.state.classNameModifier = className + '--forward';
+			}
+
+			window.setTimeout(function (vnode) {
+				vnode.children = vnode.state.childrenNext;
+				m.redraw();
+			}, 500, vnode);
+
+			window.setTimeout(function (vnode) {
+				vnode.state.classNameModifier = '';
+				m.redraw();
+			}, 1000, vnode);
+
+			vnode.state.routePath = store.currentRoutePath;
+		}
+
+		return m('div' + className + vnode.state.classNameModifier, vnode.children);
 	},
 };
 
